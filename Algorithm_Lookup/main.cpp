587948fd6660c3673bb72a753ab992ac34ab5433
Time travel <2440566839@qq.com>
2021-02-25 22:43:04 +0800

    first commit

---
+++ Algorithm_Lookup/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int Search(int a[],int n,int k);
+int main() {
+
+	int a[] = { 1,3,2,6 };
+
+	printf("%d\n",Search(a,4,2));
+	getchar();
+}
+
+int Search(int a[], int n, int k) {
+	int i;
+	for (i = 1; i <= n; ++i) {
+		if (a[i] == k) {
+			return i;
+		}
+	}
+	return 0;
+}
