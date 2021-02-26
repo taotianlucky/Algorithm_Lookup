4e11fd7b3fffa172ae49bb3bfd88a655aa31cf5c
Time travel <2440566839@qq.com>
2021-02-26 22:22:28 +0800

    forth commit

--- Algorithm_Lookup/main.cpp
+++ Algorithm_Lookup/main.cpp
@@ -22,3 +22,18 @@ int Search(int a[], int n, int k) {
 	}
 	return 0;
 }
+
+int Bsearch(int R[], int low, int high, int k) {
+	int mid;
+	while (low<=high) {
+		mid = (low + high) / 2;
+		if (R[mid] == k) {
+			return mid;
+		}else if (R[mid]>k) {
+			high = mid - 1;
+		}else {
+			low = mid + 1;
+		}
+	}
+	return 0;
+}
